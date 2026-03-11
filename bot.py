c6711bf57aad7cf3affebeaf34cf2066bc0d1c02
Omarfxbot <omar.makoubi@gmail.com>
2026-03-11 02:54:57 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import os
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 BOT_TOKEN = ("8605977902:AAFcABeU2bbnDJenJ0qLgpvBbOceWC3GzsU")
 
@@ -9,8 +9,8 @@ EXNESS_LINK = "https://one.exnessonelink.com/a/zi8w32eknv"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
-        [InlineKeyboardButton("🔵 RoboForex + بونص 30$", callback_data="robo")],
-        [InlineKeyboardButton("🟢 Exness منصة احترافية", callback_data="exness")]
+        [InlineKeyboardButton("🔵 RoboForex + بونص 30$", url=ROBO_LINK)],
+        [InlineKeyboardButton("🟢 Exness منصة احترافية", url=EXNESS_LINK)]
     ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
@@ -20,42 +20,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         reply_markup=reply_markup
     )
 
-async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
-    await query.answer()
-
-    if query.data == "robo":
-        await query.edit_message_text(
-            f"""🔵 RoboForex
-
-✅ إيداع فقط 10$
-🎁 بونص 30$ إضافي للتداول
-
-⚠️ البونص مخصص للتداول وليس للسحب المباشر.
-
-🚀 سجل من هنا:
-{ROBO_LINK}
-"""
-        )
-
-    elif query.data == "exness":
-        await query.edit_message_text(
-            f"""🟢 Exness
-
-🌍 منصة عالمية احترافية
-⚡ تنفيذ سريع + سبريد منخفض
-
-🚀 سجل من هنا:
-{EXNESS_LINK}
-"""
-        )
-
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
-
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(CallbackQueryHandler(button_handler))
-
     app.run_polling()
 
 if __name__ == "__main__":
