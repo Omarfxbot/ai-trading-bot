df4d717f8e569bdc68faf6dc39061bd1354a314b
Omarfxbot <omar.makoubi@gmail.com>
2026-03-11 02:18:58 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,34 @@
 import os
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 
-BOT_TOKEN = ("8605977902:AAFcABeU2bbnDJenJ0qLgpvBbOceWC3GzsU")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# زر البداية
+keyboard = [["🚀 ابدأ الآن"]]
+reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "مرحبا 👋\n\n"
-        "إذا بغيت تبدأ التداول بطريقة منظمة،\n"
-        "اكتب START في الخاص."
+        "👋 مرحبا بك\n\nاضغط على الزر باش نبدأو:",
+        reply_markup=reply_markup
     )
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+
+    if text == "🚀 ابدأ الآن":
+        await update.message.reply_text(
+            "🔥 ممتاز!\n\nرابط الدخول للبوت:\nhttps://t.me/OmarFX_helper_bot"
+        )
 
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
+
     app.add_handler(CommandHandler("start", start))
-    print("Bot is running...")
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+
     app.run_polling()
 
 if __name__ == "__main__":
     main()
-
