1764e97b396e3a1bddc04128de180676ea254dd8
Omarfxbot <omar.makoubi@gmail.com>
2026-03-12 13:09:56 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ DATABASE_URL = ("postgresql://postgres:IcdudqSekkFoJgLltsAHtekmWKPZFQdM@turntabl
 ROBO_LINK = "https://my.roboforex.com/en/?a=omawl"
 EXNESS_LINK = "https://one.exnessonelink.com/a/zi8w32eknv"
 VIP_CHANNEL = "@OmarSwingVIP"
+VIP_LINK = "https//t.me/OmarSwingVIP"
 
 # ===== DATABASE CONNECTION =====
 conn = psycopg2.connect(DATABASE_URL, sslmode="require")
@@ -129,6 +130,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
