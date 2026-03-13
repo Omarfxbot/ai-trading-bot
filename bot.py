40068182f017f0596c06ea9e998db0ea4abdb4f4
Omarfxbot <omar.makoubi@gmail.com>
2026-03-13 00:52:12 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -163,7 +163,8 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # ================= AUTO SIGNAL =================
 
 async def check_signal(context: ContextTypes.DEFAULT_TYPE):
-await context.bot.send_message(chat_id=VIP_CHANNEL, text="TEST SIGNAL")
+    await context.bot.send_message(chat_id=VIP_CHANNEL, text="TEST SIGNAL")
+
     today = datetime.utcnow().date()
 
     cur.execute("SELECT sent FROM daily_signals WHERE date = %s;", (today,))
@@ -171,7 +172,6 @@ await context.bot.send_message(chat_id=VIP_CHANNEL, text="TEST SIGNAL")
 
     if row:
         return
-
     url = f"https://api.twelvedata.com/time_series?symbol=XAU/USD&interval=15min&outputsize=200&apikey={TWELVEDATA_API_KEY}"
     response = requests.get(url).json()
 
@@ -241,6 +241,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
