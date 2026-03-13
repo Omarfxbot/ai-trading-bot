750c0c23ba28719a60b4f3798acd95d92c6f9669
Omarfxbot <omar.makoubi@gmail.com>
2026-03-13 15:05:11 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,7 +38,6 @@ CREATE TABLE IF NOT EXISTS stats (
     count INTEGER DEFAULT 0
 );
 """)
-cur.execute("DROP TABLE IF EXISTS daily_signals;")
 conn.commit()
 cur.execute("""
 CREATE TABLE IF NOT EXISTS daily_signals (
@@ -299,6 +298,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
